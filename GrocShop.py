58a04b2593b6ec76bafae385a7502bc13bd64bf2
Vlad <dogonovv@gmail.com>
2022-12-24 14:04:46 +0200

    Изменение язика

--- GrocShop.py
+++ GrocShop.py
@@ -10,11 +10,11 @@ bot = telebot.TeleBot('5961836068:AAGuMNBmcP7TAQnJLjB0vLq0PsgcQypqMiA')
 @bot.message_handler(commands = ['start'])
 def start(message):
     keybord = telebot.types.InlineKeyboardMarkup()
-    button1 = telebot.types.InlineKeyboardButton('Add new', callback_data = 'New position')
-    button2 = telebot.types.InlineKeyboardButton('Change', callback_data = 'Change position')
-    button3 = telebot.types.InlineKeyboardButton('Delete', callback_data = 'Delete position')
+    button1 = telebot.types.InlineKeyboardButton('Додати', callback_data = 'Нова позиція')
+    button2 = telebot.types.InlineKeyboardButton('Змінити', callback_data = 'Змінити позицію')
+    button3 = telebot.types.InlineKeyboardButton('Видалити', callback_data = 'Видалити позицію')
     keybord.add(button1, button2, button3)
-    bot.send_message(message.chat.id, 'Что делаем?', reply_markup = keybord)
+    bot.send_message(message.chat.id, 'Що будемо робити?', reply_markup = keybord)
     
 
 
