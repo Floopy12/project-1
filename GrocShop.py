5d0f06eb02a541161c42217b0373326b906e47a0
Vlad <dogonovv@gmail.com>
2022-12-24 14:53:29 +0200

    Начальные функции кнопок

--- GrocShop.py
+++ GrocShop.py
@@ -15,6 +15,15 @@ def start(message):
     button3 = telebot.types.InlineKeyboardButton('Видалити', callback_data = 'Видалити позицію')
     keybord.add(button1, button2, button3)
     bot.send_message(message.chat.id, 'Що будемо робити?', reply_markup = keybord)
+
+@bot.callback_query_handler(func = lambda call: True)
+def callback(call):
+    if call.data == 'Нова позиція':
+        bot.send_message(call.message.chat.id, 'Послідовно введіть: Назву, ціну та додайте фото')
+    elif call.data == 'Змінити позицію':
+        bot.send_message(call.message.chat.id, 'Послідовно введіть нові данні: Назву, ціну та додайте фото')
+    elif call.data == 'Видалити позицію':
+        bot.send_message(call.message.chat.id, 'Віберіть позицію, яку бажаете видалити')
     
 
 
@@ -22,6 +31,33 @@ def start(message):
 
 
 
+
+
+class Positions:
+    def __init__(self, name, price, photo):
+        self.name = name
+        self.price = price
+        self.photo = photo
+
+class Drinks(Positions):
+    def __init__(self, name, price, photo):
+        super().__init__(name, price, photo)
+
+class Cheeses(Positions):
+    def __init__(self, name, price, photo):
+        super().__init__(name, price, photo)
+
+class Sausages(Positions):
+    def __init__(self, name, price, photo):
+        super().__init__(name, price, photo)
+
+class Milk(Positions):
+    def __init__(self, name, price, photo):
+        super().__init__(name, price, photo)
+
+class Fruits(Positions):
+    def __init__(self, name, price, photo):
+        super().__init__(name, price, photo)
 
 
 
