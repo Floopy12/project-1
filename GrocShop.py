5a09e9085d3af1cbc9814da887847b870c8a8b8d
Vlad <dogonovv@gmail.com>
2022-12-24 18:55:13 +0200

    Отделил Admin от User

--- GrocShop.py
+++ GrocShop.py
@@ -6,25 +6,33 @@ import time
 TOKEN = '5961836068:AAGuMNBmcP7TAQnJLjB0vLq0PsgcQypqMiA'
 bot = telebot.TeleBot('5961836068:AAGuMNBmcP7TAQnJLjB0vLq0PsgcQypqMiA')
 
+ADMIN = 662765024
+
+
+
 
 @bot.message_handler(commands = ['start'])
 def start(message):
-    keybord = telebot.types.InlineKeyboardMarkup()
-    button1 = telebot.types.InlineKeyboardButton('Додати', callback_data = 'Нова позиція')
-    button2 = telebot.types.InlineKeyboardButton('Змінити', callback_data = 'Змінити позицію')
-    button3 = telebot.types.InlineKeyboardButton('Видалити', callback_data = 'Видалити позицію')
-    keybord.add(button1, button2, button3)
-    bot.send_message(message.chat.id, 'Що будемо робити?', reply_markup = keybord)
-
+    if message.chat.id == ADMIN:
+        keybord = telebot.types.InlineKeyboardMarkup()
+        button1 = telebot.types.InlineKeyboardButton('Додати', callback_data = 'Нова позиція')
+        button2 = telebot.types.InlineKeyboardButton('Змінити', callback_data = 'Змінити позицію')
+        button3 = telebot.types.InlineKeyboardButton('Видалити', callback_data = 'Видалити позицію')
+        keybord.add(button1, button2, button3)
+        bot.send_message(message.chat.id, 'Що будемо робити?', reply_markup = keybord)
+    else:
+        bot.send_message(message.chat.id, 'Перечень товаров')
 
 @bot.callback_query_handler(func = lambda call: True)
 def callback(call):
     if call.data == 'Нова позиція':
-        bot.send_message(call.message.chat.id, 'Введіть: Класс, назву, ціну, додайте фото')
+        bot.send_message(call.message.chat.id, 'Введіть: Класс, назву, ціну, додайте фото')  
+            
     elif call.data == 'Змінити позицію':
         bot.send_message(call.message.chat.id, 'Послідовно введіть нові данні:Класс, назву, ціну, додайте фото')
     elif call.data == 'Видалити позицію':
-        bot.send_message(call.message.chat.id, 'Віберіть позицію, яку бажаете видалити')
+        bot.send_message(call.message.chat.id, 'Виберіть позицію, яку бажаете видалити')
+
     
 
 
